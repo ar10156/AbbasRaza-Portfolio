408e7c9a29eeec7b3f2a04c593940bdedc051f72
Abbas Raza <98228794+ar10156@users.noreply.github.com>
2023-09-05 02:18:16 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abbas Raza",
+  title: "Web & App Developer",
+  email: "abbas.a.raza.w@gmail.com",
+  gitHub: "ar10156",
+  instagram: "_abbasraza",
+  linkedIn: "abbasraza786",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/backgroundWithWhite.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "aesthetic, black background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science and Economics student studying at Stony Brook University. I aim to utilize my background in the technology and finance sectors to create a positive impact.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
+  "Web Design",
+  "Backend Development",
   "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Microsoft 365",
+  "App Design",
+  "User Experience",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "With a proven track record of driving impactful initiatives, I am eager to contribute my expertise to the dynamic world of FinTech.";
 
 const About = () => {
   return (
